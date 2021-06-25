ce61498c857edeb1157212677f1f44b636e01949
Sanjeev-Bhuvan <sanjeevbhuvan04@gmail.com>
2021-06-25 05:44:20 +0530

    project eduford

---
+++ action_page.php
@@ -0,0 +1,19 @@
+<?php
+$firstname=$_post['firstname'];
+$lastname=$_post['lastname']
+$visitor_email=$_post['email'];
+$subject=$_post['subject'];
+$email_from='sanjeevbhuvan04@gmail.com';
+$email_subject='New Form Submission';
+$email_body="User First Name:$firstname.\n".
+            "User Last Name:$lastname.\n".
+            "User Email:$visitor_email.\n".
+            "Subject:$subject.\n";
+
+$to ='sanjeevbhuvan04@gmail.com';
+$headers = "From:$email_from \r\n";
+$headers .="Reply-To:$visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers)
+header("Location: contact.html");
+?>
+            
